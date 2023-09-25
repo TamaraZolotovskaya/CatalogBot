26552de180ac66d74be215dcc1d817152a2b2856
TamaraZolotovskaya <116792070+TamaraZolotovskaya@users.noreply.github.com>
2023-09-25 12:43:31 +0700

    First Commit

---
+++ src/main/java/com/tamara/catalogbot/CatalogBotApplication.java
@@ -0,0 +1,13 @@
+package com.tamara.catalogbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tamara/catalogbot/CatalogBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamara.catalogbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
